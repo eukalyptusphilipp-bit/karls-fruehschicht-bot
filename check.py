7a51a4499c4f55d77e78d0d333294d77ba3a8898
eukalyptusphilipp-bit <eukalyptusphilipp@gmail.com>
2026-04-28 15:43:31 +0200

    Update check.py

--- check.py
+++ check.py
@@ -3,7 +3,7 @@ import json
 import time
 import requests
 from selenium import webdriver
-from selenium.webdriver.by import By
+from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
